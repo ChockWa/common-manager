68682c37e16e112aa08c545680134d67bf176743
hezhuohua <hzh081215@qq.com>
2019-03-12 23:40:40 +0800

    菜单

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -1,104 +1,264 @@
 <template>
-    <div class="menu">
-      <tree-item :treeList="list"></tree-item>
-    </div>
+  <div>
+    <el-menu
+        default-active="0"
+        class="el-menu-vertical-demo"
+        @select="menuSelected"
+        background-color="#F0F6F6"
+        text-color="#3C3F41"
+        active-text-color="black">
+        <NavMenu :navMenus="leftMenus"></NavMenu>
+      </el-menu>
+  </div>
 </template>
 
 <script>
-import TreeItem from './../tree.item'
-
+import NavMenu from './NavMenu'
 export default {
   name: 'Menu',
-  components: {TreeItem},
-  data() {
-    return {
-      list: [
+  components:{NavMenu},
+  data(){
+    return{
+      leftMenus: [
         {
-          id: 1,
-          menuName: "项目管理",
-          level: 1,
-          childTree: [
-            {
-              id: 2,
-              menuName: "项目进度",
-              level: 2,
-              childTree: [
-                {
-                  id: 3,
-                  menuName: "项目一",
-                  level: 3,
-                  childTree: [
-                    {
-                      id: 4,
-                      menuName: "详细信息",
-                      level: 4,
-                      childTree: [
-                        {
-                          id: 5,
-                          menuName: "详细信息",
-                          level: 4,
-                          childTree: [
-                            {
-                              id: 6,
-                              menuName: "详细信息",
-                              level: 4,
-                              childTree: [
-                                {
-                                  id: 7,
-                                  menuName: "详细信息",
-                                  level: 4,
-                                  childTree: [
-                                    {
-                                      id: 8,
-                                      menuName: "详细信息",
-                                      level: 4,
-                                      childTree: [
-                                        {
-                                          id: 9,
-                                          menuName: "详细信息",
-                                          level: 4,
-                                          childTree: []
-                                        }
-                                      ]
-                                    }
-                                  ]
-                                }
-                              ]
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
+            "entity": {
+                "id": 1,
+                "parentMenuId": 0,
+                "name": "systemManage",
+                "icon": "el-icon-message\r\n",
+                "alias": "系统管理",
+                "state": "ENABLE",
+                "sort": 0,
+                "value": null,
+                "type": "NONE",
+                "discription": "用于系统管理的菜单",
+                "createUserId": 1
             },
-            {
-              id: 10,
-              menuName: "任务安排",
-              level: 2,
-              childTree: []
-            }
-          ]
+            "childs": [
+                {
+                    "entity": {
+                        "id": 3,
+                        "parentMenuId": 1,
+                        "name": "authManage",
+                        "icon": "el-icon-loading",
+                        "alias": "权限管理",
+                        "state": "ENABLE",
+                        "sort": 0,
+                        "value": "/system/auth",
+                        "type": "LINK",
+                        "discription": "用于权限管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                },
+                {
+                    "entity": {
+                        "id": 4,
+                        "parentMenuId": 1,
+                        "name": "roleManage",
+                        "icon": "el-icon-bell",
+                        "alias": "角色管理",
+                        "state": "ENABLE",
+                        "sort": 1,
+                        "value": "/system/role",
+                        "type": "LINK",
+                        "discription": "用于角色管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                },
+                {
+                    "entity": {
+                        "id": 2,
+                        "parentMenuId": 1,
+                        "name": "menuManage",
+                        "icon": "el-icon-edit",
+                        "alias": "菜单管理",
+                        "state": "ENABLE",
+                        "sort": 2,
+                        "value": "/system/menu",
+                        "type": "LINK",
+                        "discription": "用于菜单管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                },
+                {
+                    "entity": {
+                        "id": 5,
+                        "parentMenuId": 1,
+                        "name": "groupManage",
+                        "icon": "el-icon-mobile-phone\r\n",
+                        "alias": "分组管理",
+                        "state": "ENABLE",
+                        "sort": 3,
+                        "value": "/system/group",
+                        "type": "LINK",
+                        "discription": "用于分组管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                }
+            ]
         },
         {
-          id: 11,
-          menuName: "数据统计",
-          level: 1,
-          childTree: []
+            "entity": {
+                "id": 6,
+                "parentMenuId": 0,
+                "name": "userManage",
+                "icon": "el-icon-news",
+                "alias": "用户管理",
+                "state": "ENABLE",
+                "sort": 1,
+                "value": null,
+                "type": "NONE",
+                "discription": "用于用户管理的菜单",
+                "createUserId": 1
+            },
+            "childs": [
+                {
+                    "entity": {
+                        "id": 7,
+                        "parentMenuId": 6,
+                        "name": "accountManage",
+                        "icon": "el-icon-phone-outline\r\n",
+                        "alias": "帐号管理",
+                        "state": "ENABLE",
+                        "sort": 0,
+                        "value": "",
+                        "type": "NONE",
+                        "discription": "用于帐号管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": [
+                        {
+                            "entity": {
+                                "id": 14,
+                                "parentMenuId": 7,
+                                "name": "emailManage",
+                                "icon": "el-icon-sold-out\r\n",
+                                "alias": "邮箱管理",
+                                "state": "ENABLE",
+                                "sort": 0,
+                                "value": "/content/email",
+                                "type": "LINK",
+                                "discription": "用于邮箱管理的菜单",
+                                "createUserId": 1
+                            },
+                            "childs": null
+                        },
+                        {
+                            "entity": {
+                                "id": 13,
+                                "parentMenuId": 7,
+                                "name": "passManage",
+                                "icon": "el-icon-service\r\n",
+                                "alias": "密码管理",
+                                "state": "ENABLE",
+                                "sort": 1,
+                                "value": "/content/pass",
+                                "type": "LINK",
+                                "discription": "用于密码管理的菜单",
+                                "createUserId": 1
+                            },
+                            "childs": null
+                        }
+                    ]
+                },
+                {
+                    "entity": {
+                        "id": 8,
+                        "parentMenuId": 6,
+                        "name": "integralManage",
+                        "icon": "el-icon-picture",
+                        "alias": "积分管理",
+                        "state": "ENABLE",
+                        "sort": 1,
+                        "value": "/user/integral",
+                        "type": "LINK",
+                        "discription": "用于积分管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                }
+            ]
         },
         {
-          id: 12,
-          menuName: "人员管理",
-          level: 1,
-          childTree: []
+            "entity": {
+                "id": 9,
+                "parentMenuId": 0,
+                "name": "contentManage",
+                "icon": "el-icon-rank",
+                "alias": "内容管理",
+                "state": "ENABLE",
+                "sort": 2,
+                "value": null,
+                "type": "NONE",
+                "discription": "用于内容管理的菜单",
+                "createUserId": 1
+            },
+            "childs": [
+                {
+                    "entity": {
+                        "id": 10,
+                        "parentMenuId": 9,
+                        "name": "classifyManage",
+                        "icon": "el-icon-printer",
+                        "alias": "分类管理",
+                        "state": "ENABLE",
+                        "sort": 0,
+                        "value": "/content/classify",
+                        "type": "LINK",
+                        "discription": "用于分类管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                },
+                {
+                    "entity": {
+                        "id": 11,
+                        "parentMenuId": 9,
+                        "name": "articleManage",
+                        "icon": "el-icon-star-on",
+                        "alias": "文章管理",
+                        "state": "ENABLE",
+                        "sort": 1,
+                        "value": "/content/article",
+                        "type": "LINK",
+                        "discription": "用于文章管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                },
+                {
+                    "entity": {
+                        "id": 12,
+                        "parentMenuId": 9,
+                        "name": "commentManage",
+                        "icon": "el-icon-share",
+                        "alias": "评论管理",
+                        "state": "ENABLE",
+                        "sort": 2,
+                        "value": "/content/comment",
+                        "type": "LINK",
+                        "discription": "用于评论管理的菜单",
+                        "createUserId": 1
+                    },
+                    "childs": null
+                }
+            ]
         }
-      ]
+    ]
     }
+  },
+  methods:{
+    menuSelected(){}
   }
-
 }
 </script>
 
-<style lang="scss">
+<style>
+.el-menu-vertical-demo {
+  width: 100%;
+}
 </style>
--- src/views/Home1.vue
+++ src/views/Home1.vue
@@ -1,8 +1,7 @@
 <template>
-    <el-row class="tac">
-  <el-col :span="12">
-    <h5>自定义颜色</h5>
-    <el-menu
+    <el-row class="container">
+  <el-col :span="24" class="main">
+    <!-- <el-menu
       default-active="2"
       class="el-menu-vertical-demo"
       @open="handleOpen"
@@ -15,14 +14,7 @@
           <i class="el-icon-location"></i>
           <span>导航一</span>
         </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
+        <el-menu-item index="1-3">选项3</el-menu-item>
         <el-submenu index="1-4">
           <template slot="title">选项4</template>
           <el-menu-item index="1-4-1">选项1</el-menu-item>
@@ -32,22 +24,184 @@
         <i class="el-icon-menu"></i>
         <span slot="title">导航二</span>
       </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
-    </el-menu>
+    </el-menu> -->
+
+    <aside :class="collapsed?'menu-collapsed':'menu-expanded'">
+                <!--导航菜单-->
+                <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" v-bind:style=" collapsed ? {} : { 'width': '230px' } " :default-active="$route.path" class="el-menu-vertical-demo" unique-opened router v-show="!collapsed">
+                    <template v-for="(item,index) in menuData" v-if="item.hidden">
+                        <el-submenu :index="index+''" v-if="item.children.length >0" :key="index+''">
+                            <template slot="title">
+                                <i :class="item.iconCls"></i>{{item.name}}
+                            </template>
+                            <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="child.hidden">
+                                {{child.name}}
+                            </el-menu-item>
+                        </el-submenu>
+                        <el-menu-item v-if="item.children.length>0" :index="item.children[0].path" :key="item.children[0].path">
+                            <i :class="item.iconCls"></i>{{item.children[0].name}}
+                        </el-menu-item>
+                    </template>
+                </el-menu>
+                <!--导航菜单-折叠后-->
+                <ul class="el-menu el-menu-vertical-demo collapsed el-menu-collapsed" v-show="collapsed" ref="menuCollapsed">
+                    <li v-for="(item,index) in menuData" v-if="item.hidden" class="el-submenu item" :key="index">
+                        <template v-if="item.children.length>0">
+                            <div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
+                                <i :class="item.iconCls"></i>
+                            </div>
+                            <ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
+                                <li v-for="child in item.children" v-if="child.hidden" :key="child.path" class="el-menu-item" style="padding-left: 40px; color: #fff;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
+                                    {{child.name}}
+                                </li>
+                            </ul>
+                        </template>
+                        <template v-else>
+                            <li class="el-submenu">
+                                <div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)">
+                                    <i :class="item.iconCls"></i>
+                                </div>
+                            </li>
+                        </template>
+                    </li>
+                </ul>
+            </aside>
   </el-col>
 </el-row>
 </template>
 
 <script>
+let data = () => {
+        return {
+            sysName: '后台管理系统',
+            collapsed: false,
+            sysUserName: '管理员',
+            menuData: [
+        {
+          id: 1,
+          name: "项目管理",
+          level: 1,
+          path: '1',
+          children: [
+            {
+              id: 2,
+              name: "项目进度",
+              level: 2,
+              path: '2',
+              children: [
+                {
+                  id: 3,
+                  name: "项目一",
+                  level: 3,
+                  path: '3',
+                  children: [
+                    {
+                      id: 4,
+                      name: "详细信息",
+                      level: 4,
+                      path: '4',
+                      children: [
+                        {
+                          id: 5,
+                          name: "详细信息",
+                          level: 4,
+                          path: '5',
+                          children: [
+                            {
+                              id: 6,
+                              name: "详细信息",
+                              level: 4,
+                              path: '6',
+                              children: [
+                                {
+                                  id: 7,
+                                  name: "详细信息",
+                                  level: 4,
+                                  path: '7',
+                                  children: [
+                                    {
+                                      id: 8,
+                                      name: "详细信息",
+                                      level: 4,
+                                      path: '8',
+                                      children: [
+                                        {
+                                          id: 9,
+                                          name: "详细信息",
+                                          level: 4,
+                                          path: '9',
+                                          children: []
+                                        }
+                                      ]
+                                    }
+                                  ]
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              id: 10,
+              name: "任务安排",
+              level: 2,
+              path: '10',
+              children: []
+            }
+          ]
+        },
+        {
+          id: 11,
+          name: "数据统计",
+          level: 1,
+          path: '11',
+          children: []
+        },
+        {
+          id: 12,
+          name: "人员管理",
+          level: 1,
+          path: '12',
+          children: []
+        }
+      ]
+        }
+    }
+
+    // let initMenu = function() {
+    //     for(let i in this.$router.options.routes) {
+    //         let root = this.$router.options.routes[i]
+    //         if(root.hidden)
+    //             continue
+    //         let children = []
+    //         for(let j in root.children) {
+    //             let item = root.children[j]
+    //             if(item.hidden)
+    //                 continue
+    //             children.push(item)
+    //         }
+
+    //         if(children.length < 1)
+    //             continue
+    //         this.menuData.push(root)
+    //         root.children = children
+    //     }
+    // }
 export default {
+    data: data,
     methods: {
+      //折叠导航栏
+            collapse: function() {
+                this.collapsed = !this.collapsed;
+            },
+            showMenu: function(i, status) {
+                this.$refs.menuCollapsed.getElementsByClassName('submenu-hook-' + i)[0].style.display = status ? 'block' : 'none';
+            },
       handleOpen(key, keyPath) {
         console.log(key, keyPath);
       },
@@ -58,6 +212,141 @@ export default {
   }
 </script>
 
-<style>
-
+<style lang="scss" scoped="scoped">
+.container {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        .header {
+            height: 60px;
+            line-height: 60px;
+            background: #545c64;
+            color: #fff;
+            .userinfo {
+                text-align: right;
+                padding-right: 35px;
+                float: right;
+                .userinfo-inner {
+                    cursor: pointer;
+                    color: #fff;
+                    img {
+                        width: 40px;
+                        height: 40px;
+                        border-radius: 20px;
+                        margin: 10px 0 10px 10px;
+                        float: right;
+                    }
+                }
+            }
+            .logo {
+                height: 60px;
+                font-size: 22px;
+                padding-left: 20px;
+                padding-right: 20px;
+                border-color: rgba(238, 241, 146, 0.3);
+                border-right-width: 1px;
+                border-right-style: solid;
+                img {
+                    width: 40px;
+                    float: left;
+                    margin: 10px 10px 10px 18px;
+                }
+                .txt {
+                    color: #fff;
+                }
+            }
+            .logo-width {
+                width: 230px;
+            }
+            .logo-collapse-width {
+                width: 60px;
+            }
+            .tools {
+                padding: 0 23px;
+                width: 14px;
+                height: 60px;
+                line-height: 60px;
+                cursor: pointer;
+            }
+        }
+        .main {
+            display: flex;
+            position: absolute;
+            top: 60px;
+            bottom: 0;
+            overflow: hidden;
+            aside {
+                flex: 0 0 230px;
+                width: 230px;
+                /*侧边菜单*/
+                .el-menu {
+                    height: 100%;
+                    background-color: #545c64;
+                    /*选中列*/
+                    .el-menu-item {
+                        background-color: #545c64;
+                    }
+                    .el-submenu__title {
+                        i {
+                            color: #fff;
+                        }
+                    }
+                }
+                .el-menu:first-child {
+                    overflow-y: auto !important;
+                }
+                .el-menu-collapsed li:hover {
+                    background-color: #434a50;
+                }
+                /*当前选中菜单*/
+                .is-opened {
+                    color: #fff;
+                    background-color: #fff;
+                }
+                .collapsed {
+                    width: 60px;
+                    .item {
+                        position: relative;
+                    }
+                    .submenu {
+                        position: absolute;
+                        top: 0;
+                        left: 60px;
+                        z-index: 99999;
+                        height: auto;
+                        display: none;
+                    }
+                }
+            }
+            .menu-collapsed {
+                flex: 0 0 60px;
+                width: 60px;
+            }
+            .menu-expanded {
+                flex: 0 0 230px;
+                width: 230px;
+            }
+            .content-container {
+                flex: 1;
+                overflow-y: scroll;
+                padding: 20px;
+                background-color: #d3d7d4;
+                .breadcrumb-container {
+                    display: none;
+                    .title {
+                        width: 200px;
+                        float: left;
+                        color: #475669;
+                    }
+                    .breadcrumb-inner {
+                        float: right;
+                    }
+                }
+                .content-wrapper {
+                    box-sizing: border-box;
+                }
+            }
+        }
+    }
 </style>
